0f3d183f223404a33168aebe152d3a2c78171d26
İlyas Bilgihan <ilyasbilgihan@gmail.com>
2024-09-04 11:49:48 +0300

    fix: bottomsheet BackPress on open state

--- components/BottomSheet.tsx
+++ components/BottomSheet.tsx
@@ -1,8 +1,10 @@
-import React, { forwardRef, useCallback, useMemo } from 'react';
+import React, { forwardRef, useCallback, useMemo, useState } from 'react';
 import { BottomSheetFooterProps, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
 import { BottomSheetBackdropProps } from '@gorhom/bottom-sheet';
 import Animated, { Extrapolation, interpolate, useAnimatedStyle } from 'react-native-reanimated';
 import { useGlobalContext } from '~/context/GlobalProvider';
+import { useFocusEffect } from 'expo-router';
+import { BackHandler } from 'react-native';
 
 const CustomBackdrop = ({ animatedIndex, style }: BottomSheetBackdropProps) => {
   // animated variables
@@ -35,9 +37,28 @@ const BottomSheet = forwardRef<
 >(({ footerComponent, snapPoints, children }, ref) => {
   const snapPts = useMemo(() => snapPoints, []);
 
+  const [currentState, setCurrentState] = useState<number>(-1);
   const handleSheetChanges = useCallback((index: number) => {
-    console.log('handleSheetChanges', index);
+    setCurrentState(index);
   }, []);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      const onBackPress = () => {
+        if (currentState != -1 && ref) {
+          //@ts-ignore
+          ref?.current?.close();
+          return true;
+        } else {
+          return false;
+        }
+      };
+
+      BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+      return () => BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+    }, [ref, currentState])
+  );
 
   const { ifLight } = useGlobalContext();
 
